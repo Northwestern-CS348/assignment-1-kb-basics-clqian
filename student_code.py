fd1eb02097a75a1b43bb521eebfb8a7727213f85
Clarissa Qian <clarissaqianx@gmail.com>
2019-01-24 21:59:04 -0600

    basic KB

--- student_code.py
+++ student_code.py
@@ -7,6 +7,7 @@ class KnowledgeBase(object):
     def __init__(self, facts=[], rules=[]):
         self.facts = facts
         self.rules = rules
+        self.factset = set(facts)
 
     def __repr__(self):
         return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
@@ -23,7 +24,10 @@ class KnowledgeBase(object):
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
-        print("Asserting {!r}".format(fact))
+        #print("Asserting {!r}".format(fact))
+        if isinstance(fact, Fact):
+            self.facts.append(fact)
+
         
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
@@ -34,4 +38,11 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
-        print("Asking {!r}".format(fact))
+        #print("Asking {!r}".format(fact))
+        r = ListOfBindings()
+        for f in self.facts:
+            m = match(f.statement, fact.statement)
+            if m:
+                r.add_bindings(m)
+                print(r[0])
+        return r if r else False
